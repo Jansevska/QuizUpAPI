69d2c9b6ac4112d112355e3d7f82e6325b4fb826
Emili <ejanchevis@gmail.com>
2023-11-20 09:03:12 -1000

    user ability to interact with questions, write their own answers and check if it's correct or incorrect

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-// import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form'
 
 
 type QuestionType = {
@@ -18,42 +18,42 @@ type Props = {
 
 export default function QuestionCard({ question }: Props) {
 
-    const [showAnswer, setShowAnswer] = useState(false);
-    // const [guess, setGuess] = useState('');
-    // const [correct, setCorrect] = useState(false);
-    // const [formSubitted, setFormSubmitted] = useState(false);
+    // const [showAnswer, setShowAnswer] = useState(false);
+    const [guess, setGuess] = useState('');
+    const [correct, setCorrect] = useState(false);
+    const [formSubitted, setFormSubmitted] = useState(false);
 
-    // const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    //     setGuess(e.target.value);
-    // }
+    const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setGuess(e.target.value);
+    }
 
-    // const handleFormSubmit = (e:React.FormEvent) => {
-    //     e.preventDefault();
-    //     setFormSubmitted(true)
-    //     if (guess.toLowerCase() === question.answer.toLowerCase()) {
-    //         setCorrect(true);
-    //     } else {
-    //         setCorrect(false);
-    //     }
-    // }
+    const handleFormSubmit = (e:React.FormEvent) => {
+        e.preventDefault();
+        setFormSubmitted(true)
+        if (guess.toLowerCase() === question.answer.toLowerCase()) {
+            setCorrect(true);
+        } else {
+            setCorrect(false);
+        }
+    }
 
     return (
         <Card className='my-3'>
             <Card.Body>
                 <Card.Title>{ question.question }</Card.Title>
-                {showAnswer && <Card.Text>{question.answer}</Card.Text>}
+                {/* {showAnswer && <Card.Text>{question.answer}</Card.Text>}
                 <Button variant={showAnswer ? 'danger':'success'}onClick={() => setShowAnswer(!showAnswer)}>
                     {showAnswer ? 'Hide Answer' : 'Reveal Answer'}
-                </Button>
-                {/* <Form onSubmit={handleFormSubmit}>
+                </Button> */}
+                <Form onSubmit={handleFormSubmit}>
                     <Form.Control name='guess' placeholder='Enter Your Guess' value={guess} onChange={handleInputChange} />
                     <Button type='submit'>Check Guess</Button>
                 </Form>
-                { formSubitted && correct ? (
+                { formSubitted ? correct ? (
                     <h4>Congratulations you have the correct answer!</h4>
                 ) : (
                     <h4>You are incorrect. The correct answer is {question.answer}</h4>
-                ) : null} */}
+                ) : null}
             </Card.Body>
         </Card>
     )
